d160281f956ebac58fd700a737be15af7537fc99
ThanhVt <thanhvt@ThanhVts-iMac.local>
2020-01-09 23:28:43 -0800

    Initial Commit

---
+++ RestaurantExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantExample
+//
+//  Created by ThanhVt on 1/9/20.
+//  Copyright © 2020 ThanhVt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
